07e07d951ef201be3f07702ec743ee809ef8c80d
Anton Oshurek <oshurekweb@gmail.com>
2023-08-07 12:43:42 +0200

    delete fancy slider

--- source/scripts/components/gallery-slider.js
+++
@@ -1,9 +0,0 @@
-import { Fancybox } from '@fancyapps/ui';
-
-const initGallerySlider = () => {
-	Fancybox.bind("#gallery a", {
-		groupAll: true,
-	});
-};
-
-export default initGallerySlider;
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,5 @@
-import initGallerySlider from './components/gallery-slider.js';
 import showGalleryBlockAnimation from './components/showGalleryBlockAnimation.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-	initGallerySlider();
 	showGalleryBlockAnimation();
 });
