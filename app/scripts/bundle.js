c04ccfbbc2c175ea124f653c61c070a1b8b36a81
AntonOshurek <oshurekweb@gmail.com>
2023-09-13 22:46:01 +0200

    add slider to inside section

--- app/scripts/bundle.js
+++ app/scripts/bundle.js
@@ -46,6 +46,37 @@ const aboutSlider = () => {
 
 /***/ }),
 
+/***/ "./src/scripts/components/inside-slider.js":
+/*!*************************************************!*\
+  !*** ./src/scripts/components/inside-slider.js ***!
+  \*************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper */ "./node_modules/swiper/swiper.mjs");
+/* harmony import */ var swiper_modules__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! swiper/modules */ "./node_modules/swiper/modules/index.mjs");
+
+
+const insideSlider = () => {
+  // eslint-disable-next-line no-unused-vars
+  const swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".inside__slider-swiper", {
+    direction: "horizontal",
+    loop: true,
+    modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation],
+    spaceBetween: 50,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (insideSlider);
+
+/***/ }),
+
 /***/ "./src/scripts/components/showGalleryBlockAnimation.js":
 /*!*************************************************************!*\
   !*** ./src/scripts/components/showGalleryBlockAnimation.js ***!
@@ -10036,11 +10067,14 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_showGalleryBlockAnimation_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/showGalleryBlockAnimation.js */ "./src/scripts/components/showGalleryBlockAnimation.js");
 /* harmony import */ var _components_about_slider_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/about-slider.js */ "./src/scripts/components/about-slider.js");
+/* harmony import */ var _components_inside_slider_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/inside-slider.js */ "./src/scripts/components/inside-slider.js");
+
 
 
 window.addEventListener('DOMContentLoaded', () => {
   (0,_components_about_slider_js__WEBPACK_IMPORTED_MODULE_1__["default"])();
   (0,_components_showGalleryBlockAnimation_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
+  (0,_components_inside_slider_js__WEBPACK_IMPORTED_MODULE_2__["default"])();
 });
 })();
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import showGalleryBlockAnimation from './components/showGalleryBlockAnimation.js';
 import aboutSlider from './components/about-slider.js';
+import insideSlider from './components/inside-slider.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 	aboutSlider();
 	showGalleryBlockAnimation();
+	insideSlider();
 });
